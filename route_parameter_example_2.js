4c68c707bbb76811a9818417b345de01e11fcb18
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-19 11:18:19 +0800

    Add my implementation of app.param() for bookId

--- route_parameter_example_2.js
+++ route_parameter_example_2.js
@@ -2,20 +2,38 @@ const express = require("express");
 const app = express();
 
 app.param("userId", function(req, res, next, userId) {
-    // do whatever you like with the userId, e.g. looking up the user profile in database
-    const userName = "Tom";
-    const user = {userId, userName}
+  // do whatever you like with the userId, e.g. looking up the user profile in database
+  const users = {
+    1: "David",
+    2: "Abdullah",
+    3: "Batya"
+  };
 
-    // and you can set new information in the request object
-    req.user = user;
+  const user = { userId, userName: users[userId] };
 
-    // now let the next handler to process the request
-    next();
+  // and you can set new information in the request object
+  req.user = user;
+
+  // now let the next handler to process the request
+  next();
+});
+
+app.param("bookId", function(req, res, next, bookId) {
+  const books = {
+    1: "Bible",
+    2: "Quran",
+    3: "Torah"
+  };
+
+  const book = books[bookId];
+  req.book = book;
+  next();
 });
 
 app.get("/users/:userId/books/:bookId", function(req, res) {
   const user = req.user;
-  res.send("Got the request for user " + user.userName);
+  const book = req.book;
+  res.send("Got the request for user " + user.userName + " for the " + book);
 });
 
 const server = app.listen(3000, function() {
