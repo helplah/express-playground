7edf0b23fb874c2d566a09b7a5898d7ee5471ebf
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-20 10:28:00 +0800

    Add console.log to see middleware functions flow

--- middleware_example.js
+++ middleware_example.js
@@ -1,18 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("tiny"));
+
+// middleware function
 app.use(function(req, res, next) {
-  console.log("common middleware function was called!");
+  console.log("1. common middleware function was called!");
   next();
+  console.log("6. this should print last");
 });
 
+// middleware function
 app.use("/users", function(req, res, next) {
-  console.log("middleware function for /users was called!");
+  console.log("2. middleware function for /users was called!");
   next();
+  console.log("5. after next from users middleware");
 });
 
+// route handlers
 app.get("/users", function(req, res) {
+  console.log("3. before resending response");
   res.send("Here is a list of users:....");
+  console.log("4. after resending response");
 });
 
 app.get("/books", function(req, res) {
